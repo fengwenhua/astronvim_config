8b3686450fb65eaa0a196774d392aaaa2223dc88
fengwenhua <13612265462@163.com>
2023-08-11 23:26:03 +0800

    add markdown

--- plugins/community.lua
+++ plugins/community.lua
@@ -11,5 +11,6 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.colorscheme.catppuccin" },
 }
